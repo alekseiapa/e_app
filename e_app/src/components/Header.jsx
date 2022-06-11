d195457dd958bc4d4f0a4e22b1efbd566870d33f
alekseiapa <aapaseev@bk.ru>
2022-06-11 12:15:09 +0800

    feat: added ref links

--- e_app/src/components/Header.jsx
+++ e_app/src/components/Header.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import "./Header.css";
+import { Link } from "react-router-dom";
 
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import StorefrontIcon from "@mui/icons-material/Storefront";
 
+import "./Header.css";
+
 export default function Header() {
   return (
     <div className="header">
-      <div className="header__logo">
-        <StorefrontIcon className="header__logoImage" fontSize="large" />
-        <h2 className="header__logoTitle">eCart</h2>
-      </div>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <div className="header__logo">
+          <StorefrontIcon className="header__logoImage" fontSize="large" />
+          <h2 className="header__logoTitle">eCart</h2>
+        </div>
+      </Link>
       <div className="header__search">
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
@@ -25,10 +29,12 @@ export default function Header() {
           <span className="nav__itemLineOne">Your</span>
           <span className="nav__itemLineTwo">Shop</span>
         </div>
-        <div className="nav__itemBasket">
-          <ShoppingBasketIcon className="itemBasket" />
-          <span className="nav__itemLineTwo nav__basketCount">0</span>
-        </div>
+        <Link to="/checkout" style={{ textDecoration: "none" }}>
+          <div className="nav__itemBasket">
+            <ShoppingBasketIcon className="itemBasket" />
+            <span className="nav__itemLineTwo nav__basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
