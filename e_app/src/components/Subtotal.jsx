49e7579c1f23390d6ba9f14e7fb39a18cfa3b649
alekseiapa <aapaseev@bk.ru>
2022-06-12 22:36:21 +0800

    feat: added Dynamic Subtotal values

--- e_app/src/components/Subtotal.jsx
+++ e_app/src/components/Subtotal.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import CurrencyFormat from "react-currency-format";
-
+import { useStateValue } from "../stateProvider.js";
+import { getCartTotal } from "../reducer.js";
 import "./Subtotal.css";
 
 export default function Subtotal() {
+  const [{ cart }, dispatch] = useStateValue();
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items): <strong>$0</strong>
+              Subtotal ({cart.length} {`item${cart.length > 1 ? "s" : ""}`}):{" "}
+              <strong>${value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -18,7 +21,7 @@ export default function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getCartTotal(cart)}
         displayType={"text"}
         thousandSeparator={true}
       />
--- e_app/src/reducer.js
+++ e_app/src/reducer.js
@@ -2,6 +2,10 @@ export const initialState = {
   cart: [],
 };
 
+export const getCartTotal = (cart) => {
+  return cart?.reduce((amount, item) => item.price + amount, 0);
+};
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
